f10ebcdda0edfaa4a2e00e3571cd10785e49e4b4
Morgan VanYperen <morgvanny@gmail.com>
2022-06-10 12:01:28 -0700

    updated comments

--- balancing.rb
+++ balancing.rb
@@ -44,15 +44,18 @@ def number_to_balance(test_string)
   # end
 
   test_string.chars.each do |char|
-    if char == "(" # any time we see an opening parenthesis, it can't have been closed yet so add 1 to open_and_not_closed
+    if char == "(" # any time the character is an opening parenthesis
+      # it can't have been closed yet because it's just now opening, so add 1 to open_and_not_closed
       open_and_not_closed += 1
-    elsif open_and_not_closed > 0 # if it wasn't an opening, it must be a closing - so now we check if we have any open that haven't been closed and subtract 1 if there are any
+    elsif open_and_not_closed > 0 # if it wasn't an opening, we know it must be a closing - so now we check if we have any open that haven't been closed
+      # subtract 1 from open_and_not_closed if there are any. this close must be closing the last unclosed open one we saw
       open_and_not_closed -= 1
-    else # we only get here if it was a closing AND there were no previous open ones that haven't been closed yet, so we add one to closed_and_not_opened
+    else # we only get here if it was a closing AND there were no previous open ones that haven't been closed yet
+      # so we add one to closed_and_not_opened. this closing one must be extra if there are no previously opened and unclosed ones
       closed_and_not_opened += 1
     end
   end
-  open_and_not_closed + closed_and_not_opened
+  open_and_not_closed + closed_and_not_opened # all unmatched parentheses, whether they were open ones that were never closed, or extra closing ones without previous unclosed opening ones
 end
 
 def test(input, expected_output)
